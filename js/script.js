77cf9c53a5fd4e09e71ce6b94fbe47cd4e82b536
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-15 15:42:48 +0100

    add: bonus

--- js/script.js
+++ js/script.js
@@ -3,19 +3,58 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      toDo: ["Riordinare", "Leggere", "Dentista", "Esercizi", "Palestra"],
-      done: [],
-      checked: false,
+      items: [
+        {
+          text: "Riordinare",
+          done: false,
+        },
+        {
+          text: "Leggere",
+          done: true,
+        },
+        {
+          text: "Dentista",
+          done: true,
+        },
+        {
+          text: "Esercizi",
+          done: false,
+        },
+        {
+          text: "aaa",
+          done: false,
+        },
+        {
+          text: "bbb",
+          done: true,
+        },
+        {
+          text: "ccc",
+          done: false,
+        },
+      ],
     };
   },
 
   methods: {
-    sendToDone: function (index, value) {
-      this.toDo.splice(index, 1);
+    // sendToDone: function (index, value) {
+    //   this.toDo.splice(index, 1);
 
-      console.log(this.toDo);
-      this.done.push(value);
-      console.log(this.done);
+    //   console.log(this.toDo);
+    //   this.done.push(value);
+    //   console.log(this.done);
+    // },
+
+    // sendToDo: function (index, value) {
+    //   this.done.splice(index, 1);
+    //   //   console.log(this.toDo);
+    //   this.toDo.push(value);
+    //   //   console.log(this.done);
+    // },
+
+    deleteItem: function (index) {
+      this.items.splice(index, 1);
+      console.log(this.items);
     },
   },
 });
